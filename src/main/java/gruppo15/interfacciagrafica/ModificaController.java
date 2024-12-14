7c64541a6a3e6d9d50ae63cdb5e2845f5a871c97
claud <claud@192.168.1.9>
2024-12-14 19:11:35 +0100

    Implementato ModificaController

--- src/main/java/gruppo15/interfacciagrafica/ModificaController.java
+++ src/main/java/gruppo15/interfacciagrafica/ModificaController.java
@@ -10,6 +10,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import gruppo15.rubrica.Contatto;
+import javafx.beans.binding.BooleanBinding;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
 
 public class ModificaController {
     @FXML
@@ -28,6 +33,8 @@ public class ModificaController {
     protected TextField secondaMailField;
     @FXML
     protected TextField terzaMailField;
+    @FXML
+    protected Button conferma;
     
     protected Contatto contatto;
     private boolean modificato = false;
@@ -46,7 +53,16 @@ public class ModificaController {
      * @param[in] contatto il contatto da modificare.
      */
     public void inizializzaModifica(Contatto contatto){
-        
+        this.contatto = contatto;
+        nomeField.setText(contatto.getNome());
+        cognomeField.setText(contatto.getCognome());
+        primoNumeroField.setText(contatto.getNumeroAt(0));
+        secondoNumeroField.setText(contatto.getNumeroAt(1));
+        terzoNumeroField.setText(contatto.getNumeroAt(2));
+        primaMailField.setText(contatto.getEmailAt(0));
+        secondaMailField.setText(contatto.getEmailAt(1));
+        terzaMailField.setText(contatto.getEmailAt(2));
+        initBindings();
     }
     
     /**
@@ -60,7 +76,7 @@ public class ModificaController {
      * @return Viene restituito il contatto. Se il contatto deve essere modificato isModificato() ritorna true.
      */    
     public Contatto getContatto(){
-        
+        return contatto;
     }
     
     /**
@@ -71,16 +87,61 @@ public class ModificaController {
     * @return Viene restituito true se il contatto mantenuto dal controller deve essere modificato , altrimenti ritorna false.
     */    
     public boolean isModificato() {
-        
+        return modificato;
     }
     
     @FXML
     protected void handleEsci(ActionEvent event) {
-        
+        chiudiFinestra();
     }
     
     @FXML
     protected void handleConferma(ActionEvent event) {
-        
+        if(aggiornaContatto()){
+            modificato = true;
+            chiudiFinestra();
+        }
+    }
+    
+    protected void mostraErrore(String titolo,String msg){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(titolo);
+        alert.setHeaderText(null);
+        alert.setContentText(msg);
+        ButtonType bottoneOk = new ButtonType("Ok");
+        alert.getButtonTypes().setAll(bottoneOk);
+        alert.show();
+    }
+    
+    protected boolean aggiornaContatto(){
+        contatto.setNome(nomeField.getText());
+        contatto.setCognome(cognomeField.getText());
+        try {
+            contatto.setNumeroAt(0, primoNumeroField.getText());
+            contatto.setNumeroAt(1, secondoNumeroField.getText());
+            contatto.setNumeroAt(2, terzoNumeroField.getText());
+        } catch (Exception ex) {
+            mostraErrore("Errore nell'inserimento del numero","Il numero inserito non è valido");
+            return false;
+        }
+        try {
+            contatto.setEmailAt(0, primaMailField.getText());
+            contatto.setEmailAt(1, secondaMailField.getText());
+            contatto.setEmailAt(2, terzaMailField.getText());
+        } catch (Exception ex) {
+            mostraErrore("Errore nell'inserimento della mail","La mail inserita non è valida");
+            return false;
+        }
+        return true;
+    }
+    
+    protected void chiudiFinestra(){
+        Stage stage = (Stage) ((nomeField).getScene().getWindow());
+        stage.close();
+    }
+    
+    protected void initBindings(){
+        BooleanBinding binding = nomeField.textProperty().isEmpty().and(cognomeField.textProperty().isEmpty());
+        conferma.disableProperty().bind(binding);
     }
 }
