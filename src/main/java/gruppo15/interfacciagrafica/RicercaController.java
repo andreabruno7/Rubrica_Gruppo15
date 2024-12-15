2bca22a15ed71040a73a65a701d6ab6ce6357bd6
andreabruno7 <a.bruno125@studenti.unisa.it>
2024-12-15 12:14:27 +0100

    Implementato RicercaController Aggiunta RicercaView

--- src/main/java/gruppo15/interfacciagrafica/RicercaController.java
+++ src/main/java/gruppo15/interfacciagrafica/RicercaController.java
@@ -6,15 +6,19 @@
  */
 package gruppo15.interfacciagrafica;
 
+import gruppo15.rubrica.Contatto;
 import gruppo15.rubrica.ElencoContatti;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 
 public class RicercaController extends TabellaController{
+    @FXML
+    private TextField ricerca;
     
     /**
      * @brief Inizializza gli elementi grafici della finestra di ricerca
@@ -28,21 +32,23 @@ public class RicercaController extends TabellaController{
      * @param[in] lista L'elenco in cui deve fare la ricerca.
      */
     public void inizializzaRicerca(ElencoContatti lista){
-        
+        inizializzaTabella(lista);
     }
     
     @FXML
-    private TextField ricerca;
-    
-    
-
-    @FXML
     private void handleRicerca(ActionEvent ev){
-        
+        tabella.setItems(elenco.ricercaContatto(ricerca.getText()));
     }
     
     @FXML
     private void handleEsci(ActionEvent ev){
-        
+        Stage stage = (Stage) ((ricerca).getScene().getWindow());
+        stage.close();
+    }
+    
+    @Override
+    protected void gestisciEliminazioneContatto(Contatto contatto){
+        elenco.removeContatto(contatto);
+        tabella.getItems().remove(contatto);
     }
 }
